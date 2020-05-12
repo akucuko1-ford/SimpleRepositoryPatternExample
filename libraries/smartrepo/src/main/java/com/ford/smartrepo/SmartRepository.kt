fb85db2f7ec6ba070edc632c926ef87d7e79604c
Kucukoz <akucuko1@MGC12Z6498LVCG.fbpld77.ford.com>
2020-05-11 20:10:17 -0400

    Initial start for generic repository pattern

--- libraries/smartrepo/src/main/java/com/ford/smartrepo/SmartRepository.kt
+++ libraries/smartrepo/src/main/java/com/ford/smartrepo/SmartRepository.kt
@@ -1,6 +1,43 @@
 package com.ford.smartrepo
 
-class SmartRepository {
+import io.reactivex.BackpressureStrategy
+import io.reactivex.Flowable
+import io.reactivex.Observable
+import io.reactivex.schedulers.Schedulers
 
-    class Adapter<VH> {}
+class SmartRepository<T>(private val adapter: Adapter<T>) {
+
+    fun getObservable(vin: String): Observable<T> = adapter.getDatabaseData(vin)
+        .subscribeOn(Schedulers.io())
+        .map { getData(it, vin) }
+        .observeOn(Schedulers.single())
+        .flatMap { resumeApiCall(vin, it).startWith(it).toFlowable(BackpressureStrategy.BUFFER) }
+        .observeOn(Schedulers.computation())
+        .toObservable()
+        .distinctUntilChanged()
+
+    private fun getData(it: List<T>, vin: String) =
+        if (it.isEmpty()) adapter.onCreateModelInstance(vin)
+        else it[0]
+
+    private fun resumeApiCall(vin: String, cacheValue: T): Observable<T> =
+        if (adapter.isCacheDataInvalid(cacheValue))
+            adapter.networkObservablesMap()[vin] ?: networkApiCall(vin, cacheValue)
+        else Observable.empty()
+
+    private fun networkApiCall(vin: String, cacheValue: T): Observable<T> =
+        adapter.getNetworkData(vin)
+            .doOnNext { adapter.saveToDatabase(it, cacheValue) }
+            .onErrorResumeNext { _: Throwable -> Observable.empty() }
+            .doFinally { adapter.networkObservablesMap().remove(vin) }
+            .apply { adapter.networkObservablesMap()[vin] = this }
+
+    interface Adapter<VH> {
+        fun onCreateModelInstance(vin: String): VH
+        fun getDatabaseData(vin: String): Flowable<List<VH>>
+        fun isCacheDataInvalid(data: VH): Boolean
+        fun getNetworkData(vin: String): Observable<VH>
+        fun saveToDatabase(newData: VH, cachedData: VH)
+        fun networkObservablesMap(): MutableMap<String, Observable<VH>>
+    }
 }
