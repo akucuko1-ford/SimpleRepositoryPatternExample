8282dea8c38739bc021ab64069bbeba71970210a
Kucukoz <akucuko1@MGC12Z6498LVCG.fbpld77.ford.com>
2020-05-14 20:19:15 -0400

    Changed string key with generic type

--- libraries/smartrepo/src/main/java/com/ford/smartrepo/SmartRepository.kt
+++ libraries/smartrepo/src/main/java/com/ford/smartrepo/SmartRepository.kt
@@ -1,56 +1,55 @@
 package com.ford.smartrepo
 
-import io.reactivex.BackpressureStrategy
 import io.reactivex.Flowable
 import io.reactivex.Observable
 import io.reactivex.schedulers.Schedulers
 
-class SmartRepository<T>(private val adapter: Adapter<T>) {
+class SmartRepository<K, T>(private val adapter: Adapter<K, T>) {
 
-    private val networkMap = mutableMapOf<String, Observable<T>>()
-    private val memoryMap = mutableMapOf<String, Flowable<List<T>>>()
+    private val networkMap = mutableMapOf<K, Observable<T>>()
+    private val memoryMap = mutableMapOf<K, Flowable<List<T>>>()
 
-    fun getObservable(vin: String): Observable<T> = getDbObservable(vin).toObservable()
+    fun getObservable(key: K): Observable<T> = getDbObservable(key).toObservable()
         .subscribeOn(Schedulers.io())
-        .map { getData(it, vin) }
+        .map { getData(it, key) }
         .observeOn(Schedulers.single())
-        .flatMap { resumeApiCall(vin, it).startWith(it) }
+        .flatMap { resumeApiCall(key, it).startWith(it) }
         .observeOn(Schedulers.computation())
         .distinctUntilChanged()
 
-    private fun getDbObservable(vin: String) =
-        memoryMap[vin] ?: adapter.getDatabaseData(vin).replay(1).refCount()
-            .apply { memoryMap[vin] = this }
+    private fun getDbObservable(key: K) =
+        memoryMap[key] ?: adapter.getDatabaseData(key).replay(1).refCount()
+            .apply { memoryMap[key] = this }
 
-    private fun getData(it: List<T>, vin: String) =
-        if (it.isEmpty()) adapter.onCreateModelInstance(vin)
-        else it[0]
+    private fun getData(items: List<T>, key: K) =
+        if (items.isEmpty()) adapter.onCreateModelInstance(key)
+        else items[0]
 
-    private fun resumeApiCall(vin: String, cacheValue: T): Observable<T> =
+    private fun resumeApiCall(key: K, cacheValue: T): Observable<T> =
         if (adapter.isCacheDataInvalid(cacheValue))
-            networkMap[vin] ?: networkApiCall(vin, cacheValue)
+            networkMap[key] ?: networkApiCall(key, cacheValue)
         else Observable.empty()
 
-    private fun networkApiCall(vin: String, cacheValue: T): Observable<T> =
-        adapter.getNetworkData(vin)
+    private fun networkApiCall(key: K, cacheValue: T): Observable<T> =
+        adapter.getNetworkData(key)
             .subscribeOn(Schedulers.io())
             .doOnNext { adapter.saveToDatabase(it, cacheValue) }
             .onErrorResumeNext { _: Throwable -> Observable.empty() }
-            .doFinally { networkMap.remove(vin) }
+            .doFinally { networkMap.remove(key) }
             .share()
-            .apply { networkMap[vin] = this }
+            .apply { networkMap[key] = this }
 
-    interface Adapter<VH> {
+    interface Adapter<KY, VH> {
 
         /*
         * SmartRepository uses this function to create default model in case of database empty
         * */
-        fun onCreateModelInstance(vin: String): VH
+        fun onCreateModelInstance(key: KY): VH
 
         /*
         * To retrieve data from database which created by the client
         * */
-        fun getDatabaseData(vin: String): Flowable<List<VH>>
+        fun getDatabaseData(key: KY): Flowable<List<VH>>
 
         /*
         * The client should decide whether the cache is valid or not
@@ -62,11 +61,11 @@ class SmartRepository<T>(private val adapter: Adapter<T>) {
         *
         * This is necessary if the cache is invalid
         * */
-        fun getNetworkData(vin: String): Observable<VH>
+        fun getNetworkData(key: KY): Observable<VH>
 
         /*
         * The client should handle the login for saving the data to database or etc.
         * */
-        fun saveToDatabase(newData: VH, cachedData: VH)
+        fun saveToDatabase(newData: VH, oldData: VH)
     }
 }
